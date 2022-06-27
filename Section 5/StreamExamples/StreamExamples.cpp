7e7026e8539cce66f5729cb9ee9139ba6b4959b3
Cédric LESUEUR <cedric.lesueur@akanea.com>
2022-06-27 15:25:52 +0200

    added examples

--- Section 5/StreamExamples/StreamExamples.cpp
+++ Section 5/StreamExamples/StreamExamples.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //When testing be sure to uncomment (Ctrl K+U) the lines you want to test, and to comment (Ctrl K+C) the parts that are not needed
+    cout << "Hello World!\n";
+
+    //cout << "Hello";
+    //cout << "World" << endl; //No line break
+
+    //cout << "Hello World!" << endl;
+    //cout << "Hello" << " World!" << endl; //Can be chained
+    //cout << "Hello" << " World!\n";
+    //cout << "Hello\nOut\nThere"; // \n is a line break
+
+    int num1;
+    int num2;
+    double num3;
+
+    //cout << "Enter an integer: ";
+    //cin >> num1;
+    //cout << "You entered: " << num1 << endl;
+
+    //cout << "Enter a first integer: ";
+    //cin >> num1;
+
+    //cout << "Enter a second integer: ";
+    //cin >> num2;
+
+    //cout << "You entered " << num1 << " and " << num2 << endl;
+
+
+    //cout << "Enter 2 integers separated with a space: ";
+    //cin >> num1 >> num2;
+    //cout << "You entered " << num1 << " and " << num2 << endl;
+
+
+    //cout << "Enter a double: ";
+    //cin >> num3;
+
+    //cout << "You entered: " << num3 << endl;
+
+    //cout << "Enter an integer: ";
+    //cin >> num1;
+
+    //cout << "Enter a double: ";
+    //cin >> num3;
+
+    //cout << "The integer is: " << num1 << endl;
+    //cout << "And the double is: " << num3 << endl;
+
+    return 0;
 }
